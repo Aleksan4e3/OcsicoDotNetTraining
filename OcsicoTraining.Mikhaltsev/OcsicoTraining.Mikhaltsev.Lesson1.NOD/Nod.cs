4ed3ee60371145fa0730bb7a4f04899e534d251f
Aleksan4e3 <aleksan4e3@gmail.com>
2020-02-04 02:48:00 +0300

    Changed the algorithm

--- OcsicoTraining.Mikhaltsev/OcsicoTraining.Mikhaltsev.Lesson1.NOD/Nod.cs
+++ OcsicoTraining.Mikhaltsev/OcsicoTraining.Mikhaltsev.Lesson1.NOD/Nod.cs
@@ -4,38 +4,16 @@ namespace OcsicoTraining.Mikhaltsev.Lesson1.NOD
 {
     public static class Nod
     {
-        static void Swap(ref int number1, ref int number2)
-        {
-            (number1, number2) = (number2, number1);
-        }
-
         public static int FindNOD(int number1, int number2)
         {
-            if (number1 == 0)
+            while (number1 != 0)
             {
-                return number2;
+                int temp = number1;
+                number1 = number2 % number1;
+                number2 = temp;
             }
 
-            if (number2 == 0)
-            {
-                return number1;
-            }
-
-            if (number1 < number2)
-            {
-                Swap(ref number1, ref number2);
-            }
-
-            int nextNumber = 1;
-
-            while (nextNumber != 0)
-            {
-                nextNumber = number1 % number2;
-                number1 = number2;
-                number2 = nextNumber;
-            }
-
-            return number1;
+            return number2;
         }
     }
 }
