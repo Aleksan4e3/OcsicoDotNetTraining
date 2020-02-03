4d4ec57d73fba227681cc1fde75d974179d6f049
Aleksan4e3 <aleksan4e3@gmail.com>
2020-02-03 22:12:31 +0300

    Changed the project structure фпфшт

---
+++ OcsicoTraining.Mikhaltsev/OcsicoTraining.Mikhaltsev.Lesson1.NOD/NOD.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace OcsicoTraining.Mikhaltsev.Lesson1.NOD
+{
+    public static class Nod
+    {
+        static void Swap(ref int a, ref int b)
+        {
+            (a, b) = (b, a);
+        }
+
+        public static int FindNOD(int number1, int number2)
+        {
+            if (number1 == 0)
+            {
+                return number2;
+            }
+
+            if (number2 == 0)
+            {
+                return number1;
+            }
+
+            if (number1 < number2)
+            {
+                Swap(ref number1, ref number2);
+            }
+
+            int nextNumber = 1;
+
+            while (nextNumber != 0)
+            {
+                nextNumber = number1 % number2;
+                number1 = number2;
+                number2 = nextNumber;
+            }
+
+            return number1;
+        }
+    }
+}
--- OcsicoTraining.Mikhaltsev/Task1/Program.cs
+++ OcsicoTraining.Mikhaltsev/Task1/Program.cs
@@ -1,59 +1,27 @@
 ﻿using System;
+using OcsicoTraining.Mikhaltsev.Lesson1.NOD;
+using static System.Console;
 
-namespace Task1
+namespace OcsicoTraining.Mikhaltsev.Lesson1.Console
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter number a:");
-            bool resultParseA = int.TryParse(Console.ReadLine(), out int a);
-            Console.WriteLine("Enter number b:");
-            bool resultParseB = int.TryParse(Console.ReadLine(), out int b);
+            WriteLine("Enter number a:");
+            bool resultParseA = int.TryParse(ReadLine(), out int a);
+            WriteLine("Enter number b:");
+            bool resultParseB = int.TryParse(ReadLine(), out int b);
 
             if (resultParseA && resultParseB)
             {
-                int nod = FindNOD(a, b);
-                Console.WriteLine($"For numbers a = {a} and b = {b} NOD = {nod}");
+                int nod = Nod.FindNOD(a, b);
+                WriteLine($"For numbers a = {a} and b = {b} NOD = {nod}");
             }
             else
             {
-                Console.WriteLine("Invalid values entered!!!");
+                WriteLine("Invalid values entered!!!");
             }
-        }
-
-        static void Swap(ref int a, ref int b)
-        {
-            (a, b) = (b, a);
-        }
-
-        static int FindNOD(int number1, int number2)
-        {
-            if (number1 == 0)
-            {
-                return number2;
-            }
-
-            if (number2 == 0)
-            {
-                return number1;
-            }
-
-            if (number1 < number2)
-            {
-                Swap(ref number1, ref number2);
-            }
-
-            int nextNumber = 1;
-
-            while (nextNumber != 0)
-            {
-                nextNumber = number1 % number2;
-                number1 = number2;
-                number2 = nextNumber;
-            }
-
-            return number1;
         }
     }
 }
