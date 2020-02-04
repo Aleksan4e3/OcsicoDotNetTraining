4827b08becd07c20c345cb670ce57f3513da07da
Aleksan4e3 <aleksan4e3@gmail.com>
2020-02-04 15:59:26 +0300

    Create Task1 (#1)
    * Create Task1
    
    * Changed the project structure
    
    * Changed the project structure фпфшт
    
    * Rename folder
    
    * Add new project
    
    * Use var
    
    * Add method for read value
    
    * Add method for parse value
    
    * Changed the method "Main"
    
    * Delete Comments
    
    * Сhanged variable names
    
    * Simplified the algorithm
    
    * Changed the algorithm
    
    * Delete NOD.cs
    
    repeated
    
    * Renamed arguments
    
    * Renamed arguments
    
    * Called RunFindGdcTask() in Main
    
    * Delete repeated file
    
    * Add NOD.cs
    
    * Renamed arguments
    
    * Changed method ReadValue
    
    * Method ParseValue was deleted
    
    * Use static import for Nod class

---
+++ OcsicoTraining.Mikhaltsev/OcsicoTraining.Mikhaltsev.Lesson1.Console/Program.cs
@@ -0,0 +1,38 @@
+﻿using static OcsicoTraining.Mikhaltsev.Lesson1.NOD.Nod;
+using static System.Console;
+
+namespace OcsicoTraining.Mikhaltsev.Lesson1.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunFindGdcTask();
+        }
+
+        private static void RunFindGdcTask()
+        {
+            var firstValue = ReadValue();
+            var secondValue = ReadValue();
+
+            var resultParseFirstValue = int.TryParse(firstValue, out var firstNumber);
+            var resultParseSecondValue = int.TryParse(secondValue, out var secondNumber);
+
+            if (resultParseFirstValue && resultParseSecondValue)
+            {
+                var nod = FindNOD(firstNumber, secondNumber);
+                WriteLine($"For numbers {firstNumber} and {secondNumber} NOD = {nod}");
+            }
+            else
+            {
+                WriteLine("Invalid values entered!!!");
+            }
+        }
+
+        static string ReadValue()
+        {
+            WriteLine("Enter number:");
+            return ReadLine();
+        }
+    }
+}
---
+++ OcsicoTraining.Mikhaltsev/OcsicoTraining.Mikhaltsev.Lesson1.NOD/NOD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OcsicoTraining.Mikhaltsev.Lesson1.NOD
+{
+    public static class Nod
+    {
+        public static int FindNOD(int firstNumber, int secondNumber)
+        {
+            while (firstNumber != 0)
+            {
+                int temp = firstNumber;
+                firstNumber = secondNumber % firstNumber;
+                secondNumber = temp;
+            }
+
+            return secondNumber;
+        }
+    }
+}
