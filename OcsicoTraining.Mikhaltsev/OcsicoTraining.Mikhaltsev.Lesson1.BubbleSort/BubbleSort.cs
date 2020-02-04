95e9b3fbbfe731bb16fe8968b78f8e58cf1a9d0d
Aleksan4e3 <aleksan4e3@gmail.com>
2020-02-04 18:58:24 +0300

    Сreated methods for sorting

--- OcsicoTraining.Mikhaltsev/OcsicoTraining.Mikhaltsev.Lesson1.BubbleSort/BubbleSort.cs
+++ OcsicoTraining.Mikhaltsev/OcsicoTraining.Mikhaltsev.Lesson1.BubbleSort/BubbleSort.cs
@@ -12,5 +12,37 @@ namespace OcsicoTraining.Mikhaltsev.Lesson1.BubbleSort
         static int GetMaxElement(int[] array) => array.Max();
 
         static int GetMinElement(int[] array) => array.Min();
+
+        static void Swap(int[] a, int[] b) => (a, b) = (b, a);
+
+        static int[][] SortAsc(int[][] array, Func<int[], int> sortOption)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (sortOption(array[i]) > sortOption(array[j]))
+                    {
+                        Swap(array[i], array[j]);
+                    }
+                }
+            }
+            return array;
+        }
+
+        static int[][] SortDesc(int[][] array, Func<int[], int> sortOption)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (sortOption(array[i]) < sortOption(array[j]))
+                    {
+                        Swap(array[i], array[j]);
+                    }
+                }
+            }
+            return array;
+        }
     }
 }
