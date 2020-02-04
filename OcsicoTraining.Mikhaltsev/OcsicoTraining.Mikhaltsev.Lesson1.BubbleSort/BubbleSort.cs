58b7ca20a538945ce657fd00c76f22f586a4dbf6
Aleksan4e3 <aleksan4e3@gmail.com>
2020-02-04 18:29:32 +0300

    Сreated methods for finding Sum, Min, Max

---
+++ OcsicoTraining.Mikhaltsev/OcsicoTraining.Mikhaltsev.Lesson1.BubbleSort/BubbleSort.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OcsicoTraining.Mikhaltsev.Lesson1.BubbleSort
+{
+    public static class BubbleSort
+    {
+        static int GetSumElements(int[] array) => array.Sum();
+
+        static int GetMaxElement(int[] array) => array.Max();
+
+        static int GetMinElement(int[] array) => array.Min();
+    }
+}
