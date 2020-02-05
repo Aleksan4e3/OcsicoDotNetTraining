19713909a833106fa841c445ee21b9f39efe618d
Aleksan4e3 <aleksan4e3@gmail.com>
2020-02-05 18:25:30 +0300

    Installed EditorConfig and Create console project Presentation

---
+++ OcsicoTraining.Mikhaltsev/OcsicoTraining.Mikhaltsev.Lesson2.Presentation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OcsicoTraining.Mikhaltsev.Lesson2.Presentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
