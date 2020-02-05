6cc6375cdb888c1464356a8317659d5c54fc3035
Aleksan4e3 <aleksan4e3@gmail.com>
2020-02-05 21:54:19 +0300

    Compared the work of methods

--- OcsicoTraining.Mikhaltsev/OcsicoTraining.Mikhaltsev.Lesson2.Presentation/Program.cs
+++ OcsicoTraining.Mikhaltsev/OcsicoTraining.Mikhaltsev.Lesson2.Presentation/Program.cs
@@ -1,9 +1,16 @@
-﻿using System;
+using System;
+using static OcsicoTraining.Mikhaltsev.Lesson2.RootPowerN.NewtonSoft;
 
 namespace OcsicoTraining.Mikhaltsev.Lesson2.Presentation
 {
     internal class Program
     {
-        private static void Main() => Console.WriteLine("Hello World!");
+        private static void Main() => RunRootPowerNTask();
+
+        private static void RunRootPowerNTask()
+        {
+            Console.WriteLine($"Calculated by Newton`s method: {CalculateRootPowerN(3, 28, 0.0000001)}");
+            Console.WriteLine($"Calculated by Math.Pow: {Math.Pow(28, 1d / 3)}");
+        }
     }
 }
