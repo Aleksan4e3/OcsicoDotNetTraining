6351ad2c5b9656294bcdb1d461193df27939c001
Aleksan4e3 <aleksan4e3@gmail.com>
2020-02-05 10:56:06 +0300

    Am lesson1 task3 (#2)
    * Сreated methods for finding Sum, Min, Max
    
    * Сreated methods for sorting
    
    * Added UI for testing BubbleSort
    
    * Remooved methods Sum, Min, Max, Swap.
    Сombined SortAsc and SortDesc methods into one
    
    * Сhanged ternary operation to if
    
    * Simplified method Sort

---
+++ OcsicoTraining.Mikhaltsev/OcsicoTraining.Mikhaltsev.Lesson1.BubbleSort/BubbleSort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OcsicoTraining.Mikhaltsev.Lesson1.BubbleSort
+{
+    public static class BubbleSort
+    {
+        public static int[][] Sort(int[][] array, Func<int[], int[], bool> sortOption)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (sortOption(array[i], array[j]))
+                    {
+                        (array[i], array[j]) = (array[j], array[i]);
+                    }
+                }
+            }
+
+            return array;
+        }
+    }
+}
