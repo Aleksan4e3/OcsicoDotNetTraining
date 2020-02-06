743456f37e21ceadb7bcfe676913d1ccff294d53
Vadim <mailvadimprokopchuk@gmail.com>
2020-02-06 13:56:24 +0300

    Presentation layer for lesson 4 (#7)

---
+++ OcsicoTraining.Mikhaltsev/OcsicoTraining.Mikhaltsev.Lesson4.Presentation/Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace OcsicoTraining.Mikhaltsev.Lesson4.Presentation
+{
+    internal class Program
+    {
+        private static void Main() => Console.WriteLine("Hello World!");
+        
+    }
+}
