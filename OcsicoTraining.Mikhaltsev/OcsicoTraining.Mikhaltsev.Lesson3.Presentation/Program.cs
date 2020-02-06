2fcd53d55c31169ba05b6f71fd60aa8e65622709
Vadim <mailvadimprokopchuk@gmail.com>
2020-02-06 13:50:07 +0300

    Presentation layer for lesson 3 (#6)

---
+++ OcsicoTraining.Mikhaltsev/OcsicoTraining.Mikhaltsev.Lesson3.Presentation/Program.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace OcsicoTraining.Mikhaltsev.Lesson3.Presentation
+{
+    internal class Program
+    {
+        private static void Main() => Console.WriteLine("Hello World!");
+    }
+}
