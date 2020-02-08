88a454a18b4901c5a7d61e78aeb416c47b5881b7
Aleksan4e3 <aleksan4e3@gmail.com>
2020-02-08 23:03:51 +0300

    Fixed UnitTests for Newton method

--- OcsicoTraining.Mikhaltsev/OcsicoTraining.Mikhaltsev.Lesson4.Tests/NewtonMethodUnitTests.cs
+++ OcsicoTraining.Mikhaltsev/OcsicoTraining.Mikhaltsev.Lesson4.Tests/NewtonMethodUnitTests.cs
@@ -1,5 +1,6 @@
 using System;
 using NUnit.Framework;
+using OcsicoTraining.Mikhaltsev.Lesson2.RootPowerN;
 using static OcsicoTraining.Mikhaltsev.Lesson2.RootPowerN.NewtonMethod;
 
 namespace OcsicoTraining.Mikhaltsev.Lesson4.Tests
@@ -7,19 +8,45 @@ namespace OcsicoTraining.Mikhaltsev.Lesson4.Tests
     [TestFixture]
     public class NewtonMethodUnitTests
     {
+        private readonly Random random = new Random();
+
         [Test]
         public void PrecisionRootPowerNHigherThenGiven_InputRoot3number28precision001_ReturnTrue()
         {
-            var actual = Math.Abs(CalculateRootPowerN(3, 28, 0.001) - Math.Pow(28, 1d / 3)) < 0.001;
-            Assert.IsTrue(actual, $"Precision Newton`s method {actual} higher then 0.001");
+            //arrange
+            var expected = Math.Pow(28, 1d / 3);
+            var delta = 0.001;
+
+            //act
+            var actual = CalculateRootPowerN(3, 28, 0.001);
+
+            //assert
+            Assert.AreEqual(expected, actual, delta, $"Root power 3 of 28 is equals {expected}");
         }
 
         [Test]
-        public void RootPowerN_InputRoot4NumberMinus30_ExceptionShould() =>
-            Assert.Throws<ArgumentException>(() => CalculateRootPowerN(4, -30));
+        public void RootPowerN_InputRoot4AndNegativeNumber_ExceptionShould()
+        {
+            var inputValue = random.Next(int.MinValue, -1);
+            _ = Assert.Throws<NumberArgumentException>(() => CalculateRootPowerN(4, inputValue),
+                "Should be NumberArgumentException");
+        }
+
 
         [Test]
-        public void RootPowerN_InputRootMinus3_ExceptionShould() =>
-            Assert.Throws<ArgumentException>(() => CalculateRootPowerN(-3, 27));
+        public void RootPowerN_InputRootLessThan1_ExceptionShould()
+        {
+            var inputValue = random.Next(int.MinValue, 1);
+            _ = Assert.Throws<PowerArgumentException>(() => CalculateRootPowerN(inputValue, 10),
+                "Should be PowerArgumentException");
+        }
+
+        [Test]
+        public void RootPowerN_InputNegativePrecision_ExceptionShould()
+        {
+            var inputValue = random.Next(int.MinValue, -1);
+            _ = Assert.Throws<PrecisionArgumentException>(() => CalculateRootPowerN(4, 46, inputValue),
+                "Should be PrecisionArgumentException");
+        }
     }
 }
