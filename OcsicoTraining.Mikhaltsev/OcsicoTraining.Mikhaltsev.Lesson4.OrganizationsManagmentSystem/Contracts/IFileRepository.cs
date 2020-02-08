d8fec264273cbc3798cf591c5f4dc653c02eb91d
Aleksan4e3 <aleksan4e3@gmail.com>
2020-02-08 16:42:52 +0300

    Created contracts

---
+++ OcsicoTraining.Mikhaltsev/OcsicoTraining.Mikhaltsev.Lesson4.OrganizationsManagmentSystem/Contracts/IFileRepository.cs
@@ -0,0 +1,6 @@
+namespace OcsicoTraining.Mikhaltsev.Lesson4.OrganizationsManagmentSystem.Contracts
+{
+    public interface IFileRepository<T> : IRepository<T> where T : class
+    {
+    }
+}
---
+++ OcsicoTraining.Mikhaltsev/OcsicoTraining.Mikhaltsev.Lesson4.OrganizationsManagmentSystem/Contracts/IMemoryRepository.cs
@@ -0,0 +1,6 @@
+namespace OcsicoTraining.Mikhaltsev.Lesson4.OrganizationsManagmentSystem.Contracts
+{
+    public interface IMemoryRepository<T> : IRepository<T> where T : class
+    {
+    }
+}
