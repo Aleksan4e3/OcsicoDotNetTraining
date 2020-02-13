42b7da42eb4b024c052d668771ac48ead70e827b
Aleksan4e3 <aleksan4e3@gmail.com>
2020-02-13 19:03:50 +0300

    Finished

--- OcsicoTraining.Mikhaltsev/OcsicoTraining.Mikhaltsev.Lesson4.Presentation/Program.cs
+++ OcsicoTraining.Mikhaltsev/OcsicoTraining.Mikhaltsev.Lesson4.Presentation/Program.cs
@@ -12,79 +12,33 @@ namespace OcsicoTraining.Mikhaltsev.Lesson4.Presentation
 {
     internal class Program
     {
-        private static void Main() => Initialize();
-
-        private static void Initialize()
+        private static void Main()
         {
-            //when testing, FILES with entities DON`T delete, but DELETE CONTENTS inside the file
-            var builder = new ContainerBuilder();
+            var orgService = GetServiceProvider().GetService<IOrganizationService>();
+            var empService = GetServiceProvider().GetService<IEmployeeService>();
+            var roleService = GetServiceProvider().GetService<IRoleService>();
 
-            _ = builder.RegisterType<RoleRepository>().As<IRoleRepository>();
+            var role1 = new Role { Id = 1, Name = "Developer" };
+            var role2 = new Role { Id = 2, Name = "QA" };
+            var role3 = new Role { Id = 3, Name = "Manager" };
 
+            roleService.AddRole(role1);
+            roleService.AddRole(role2);
+            roleService.AddRole(role3);
 
-          //  _ = builder.RegisterGeneric(typeof(FileRepository<>)).As(typeof(IRepository<>));
-            _ = builder.RegisterType<OrganizationService>();
-            _ = builder.RegisterType<EmployeeService>();
-            _ = builder.RegisterType<RoleService>();
+            orgService.AddOrganization(1, "Ocsico");
+            orgService.AddOrganization(2, "Microsoft");
 
-            var container = builder.Build();
+            var emp1 = empService.CreateEmployee(1, "Alex");
+            var emp2 = empService.CreateEmployee(2, "Ivan");
+            var emp3 = empService.CreateEmployee(3, "Vadim");
 
-            using (var scope = container.BeginLifetimeScope())
-            {
-                var orgService = scope.Resolve<OrganizationService>();
-                var empService = scope.Resolve<EmployeeService>();
-                var roleService = scope.Resolve<RoleService>();
-
-                var role1 = new Role { Id = 1, Name = "Developer" };
-                var role2 = new Role { Id = 2, Name = "QA" };
-                var role3 = new Role { Id = 3, Name = "Manager" };
-
-                roleService.AddRole(role1);
-                roleService.AddRole(role2);
-                roleService.AddRole(role3);
-
-                role2.Name = "Lead";
-                roleService.UpdateRole(role2);
-                roleService.RemoveRole(role3);
-
-                orgService.AddOrganization(1, "Ocsico");
-                orgService.AddOrganization(2, "Microsoft");
-
-                var emp1 = empService.CreateEmployee(1, "Alex");
-                var emp2 = empService.CreateEmployee(2, "Ivan");
-                var emp3 = empService.CreateEmployee(3, "Vadim");
-
-                orgService.AddEmployee(1, emp1);
-                orgService.AddEmployee(1, emp2);
-                orgService.AddEmployee(2, emp3);
-
-                orgService.AssignNewRole(1, 1, 2);
-                orgService.AssignNewRole(1, 2, 1);
-                orgService.AssignNewRole(2, 3, 1);
-                orgService.AssignNewRole(1, 1, 1, 2);
-                empService.RemoveEmployee(1);
-                emp2.Name = "Updated";
-                empService.UpdateEmployee(emp2);
-                orgService.RemoveEmployee(2, 3);
-
-                var roles = roleService.GetAllRoles();
-                foreach (var role in roles)
-                {
-                    Console.WriteLine($"{role.Id} {role.Name}");
-                }
-            }
+            orgService.AddEmployee(1, emp1, 2);
+            orgService.AddEmployee(1, emp2, 1);
+            orgService.AddEmployee(2, emp3, 3);
         }
 
-        private static void RunUserPrintTask()
-        {
-            var user = new User { Name = "Alex", PhoneNumber = "+375(44)744-52-41", Salary = 1000 };
-
-            Console.WriteLine(user.PrintInfo(new NamePrinter()));
-            Console.WriteLine(user.PrintInfo(new NameSalaryPrinter()));
-            Console.WriteLine(user.PrintInfo(new AllUserInfoPrinter()));
-        }
-
-        private static void RunServicesTask()
+        private static IServiceProvider GetServiceProvider()
         {
             var serviceCollection = new ServiceCollection();
             var containerBuilder = new ContainerBuilder();
@@ -94,7 +48,22 @@ namespace OcsicoTraining.Mikhaltsev.Lesson4.Presentation
             _ = containerBuilder.RegisterType<EmployeeRepository>().As<IEmployeeRepository>();
             _ = containerBuilder.RegisterType<OrganizationRepository>().As<IOrganizationRepository>();
             _ = containerBuilder.RegisterType<EmployeeOrganizationRoleRepository>().As<IEmployeeOrganizationRoleRepository>();
+            _ = containerBuilder.RegisterType<RoleService>().As<IRoleService>();
+            _ = containerBuilder.RegisterType<EmployeeService>().As<IEmployeeService>();
+            _ = containerBuilder.RegisterType<OrganizationService>().As<IOrganizationService>();
 
+            var container = containerBuilder.Build();
+
+            return new AutofacServiceProvider(container);
+        }
+
+        private static void RunUserPrintTask()
+        {
+            var user = new User { Name = "Alex", PhoneNumber = "+375(44)744-52-41", Salary = 1000 };
+
+            Console.WriteLine(user.PrintInfo(new NamePrinter()));
+            Console.WriteLine(user.PrintInfo(new NameSalaryPrinter()));
+            Console.WriteLine(user.PrintInfo(new AllUserInfoPrinter()));
         }
     }
 }
