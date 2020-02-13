f9267b325b970b4579528d269aef26f27c45a407
Aleksan4e3 <aleksan4e3@gmail.com>
2020-02-13 12:07:21 +0300

    Implemented RoleService

---
+++ OcsicoTraining.Mikhaltsev/OcsicoTraining.Mikhaltsev.Lesson4.OrganizationsManagmentSystem/Services/RoleService.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using OcsicoTraining.Mikhaltsev.Lesson4.OrganizationsManagmentSystem.Contracts;
+using OcsicoTraining.Mikhaltsev.Lesson4.OrganizationsManagmentSystem.Models;
+using OcsicoTraining.Mikhaltsev.Lesson4.OrganizationsManagmentSystem.Repositories;
+
+namespace OcsicoTraining.Mikhaltsev.Lesson4.OrganizationsManagmentSystem.Services
+{
+    public class RoleService
+    {
+        private readonly IRepository<Role> roleRepository;
+
+        public RoleService() => roleRepository = new MemoryRepository();
+
+        public void AddRole(Role role) => roleRepository.Add(role);
+
+        public void RemoveRole(Role role) => roleRepository.Remove(role);
+
+        public void UpdateRole(Role role) => roleRepository.Update(role);
+
+        public List<Role> GetAllRoles() => roleRepository.GetAll();
+    }
+}
--- OcsicoTraining.Mikhaltsev/OcsicoTraining.Mikhaltsev.Lesson4.Presentation/Program.cs
+++ OcsicoTraining.Mikhaltsev/OcsicoTraining.Mikhaltsev.Lesson4.Presentation/Program.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using Autofac;
 using OcsicoTraining.Mikhaltsev.Lesson4.OrganizationsManagmentSystem.Contracts;
 using OcsicoTraining.Mikhaltsev.Lesson4.OrganizationsManagmentSystem.Models;
@@ -15,13 +14,13 @@ namespace OcsicoTraining.Mikhaltsev.Lesson4.Presentation
 
         private static void Initialize()
         {
+            //when testing, FILES with entities DON`T delete, but DELETE CONTENTS inside the file
             var builder = new ContainerBuilder();
 
             _ = builder.RegisterGeneric(typeof(FileRepository<>)).As(typeof(IRepository<>));
             _ = builder.RegisterType<OrganizationService>();
             _ = builder.RegisterType<EmployeeService>();
-            _ = builder.RegisterAssemblyTypes(typeof(IRepository<>).Assembly)
-                .AsClosedTypesOf(typeof(IRepository<>));
+            _ = builder.RegisterType<RoleService>();
 
             var container = builder.Build();
 
@@ -29,12 +28,23 @@ namespace OcsicoTraining.Mikhaltsev.Lesson4.Presentation
             {
                 var orgService = scope.Resolve<OrganizationService>();
                 var empService = scope.Resolve<EmployeeService>();
-                var memRepository = scope.Resolve<MemoryRepository>();
+                var roleService = scope.Resolve<RoleService>();
+
+                var role1 = new Role { Id = 1, Name = "Developer" };
+                var role2 = new Role { Id = 2, Name = "QA" };
+                var role3 = new Role { Id = 3, Name = "Manager" };
+
+                roleService.AddRole(role1);
+                roleService.AddRole(role2);
+                roleService.AddRole(role3);
+
+                role2.Name = "Lead";
+                roleService.UpdateRole(role2);
+                roleService.RemoveRole(role3);
 
                 orgService.AddOrganization(1, "Ocsico");
                 orgService.AddOrganization(2, "Microsoft");
-                //var role1 = new Role { Id = 1, Name = "Developer" };
-                //var role2 = new Role { Id = 2, Name = "QA" };
+
                 var emp1 = empService.CreateEmployee(1, "Alex");
                 var emp2 = empService.CreateEmployee(2, "Ivan");
                 var emp3 = empService.CreateEmployee(3, "Vadim");
@@ -46,12 +56,13 @@ namespace OcsicoTraining.Mikhaltsev.Lesson4.Presentation
                 orgService.AssignNewRole(1, 1, 2);
                 orgService.AssignNewRole(1, 2, 1);
                 orgService.AssignNewRole(2, 3, 1);
-                orgService.AssignNewRole(1,1,1,2);
+                orgService.AssignNewRole(1, 1, 1, 2);
                 empService.RemoveEmployee(1);
                 emp2.Name = "Updated";
                 empService.UpdateEmployee(emp2);
+                orgService.RemoveEmployee(2, 3);
 
-                var roles = memRepository.GetAll();
+                var roles = roleService.GetAllRoles();
                 foreach (var role in roles)
                 {
                     Console.WriteLine($"{role.Id} {role.Name}");
