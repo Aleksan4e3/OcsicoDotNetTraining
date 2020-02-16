a56fd9c5a2386969f7f4077c251fa0642756ac1e
Aleksan4e3 <aleksan4e3@gmail.com>
2020-02-16 21:36:35 +0300

    Am lesson6 task4 (#13)
    * Implemented Fibonacci method and tests for it
    
    * Fixed 3rd test
    
    * Changed .editorConfig
    
    * Changed 987 to 377
    
    * Changed variable names, fixed small bags
    
    * Changed method names
    
    * Changed project names
    
    * Changed namespace names
    
    * Fixed
    
    * Changed method name

---
+++ OcsicoTraining.Mikhaltsev/OcsicoTraining.Mikhaltsev.Lesson6.Presentation/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using static OcsicoTraining.Mikhaltsev.Lesson6.FibonacciNumbersGenerator.FibonacciNumbersGenerator;
+
+namespace OcsicoTraining.Mikhaltsev.Lesson6.Presentation
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            TaskFibonacciNumbersRun();
+            Console.ReadKey();
+        }
+
+        private static void TaskFibonacciNumbersRun()
+        {
+            var fibonacciNumbers = Generate(15);
+
+            foreach (var number in fibonacciNumbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
