7c93cf96c6c337a0f07b3ec98e83dca4a4a21248
Aleksan4e3 <aleksan4e3@gmail.com>
2020-02-26 11:33:43 +0300

    Deleted unnecessary functionality

--- OcsicoTraining.Mikhaltsev/OcsicoTraining.Mikhaltsev.Lesson4.OrganizationsManagmentSystem/DbContexts/AppDbContext.cs
+++ OcsicoTraining.Mikhaltsev/OcsicoTraining.Mikhaltsev.Lesson4.OrganizationsManagmentSystem/DbContexts/AppDbContext.cs
@@ -1,16 +1,10 @@
 using System.Reflection;
 using Microsoft.EntityFrameworkCore;
-using OcsicoTraining.Mikhaltsev.Lesson4.OrganizationsManagmentSystem.Models;
 
 namespace OcsicoTraining.Mikhaltsev.Lesson4.OrganizationsManagmentSystem.DbContexts
 {
     public class AppDbContext : DbContext
     {
-        public DbSet<EmployeeRole> EmployeeRoles { get; set; }
-        public DbSet<Employee> Employees { get; set; }
-        public DbSet<Role> Roles { get; set; }
-        public DbSet<Organization> Organizations { get; set; }
-
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder) =>
