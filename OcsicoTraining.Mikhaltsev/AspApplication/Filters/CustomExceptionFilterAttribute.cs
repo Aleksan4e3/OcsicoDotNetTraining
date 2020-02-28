36c7d1ded7bcd823c17467992e7d79d3a5a658b9
Aleksan4e3 <aleksan4e3@gmail.com>
2020-02-28 12:09:33 +0300

    Added exception filter

---
+++ OcsicoTraining.Mikhaltsev/AspApplication/Filters/CustomExceptionFilterAttribute.cs
@@ -0,0 +1,31 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using OcsicoTraining.Mikhaltsev.Lesson9.AspOrganizations.Models;
+
+namespace OcsicoTraining.Mikhaltsev.Lesson9.AspOrganizations.Filters
+{
+    public class CustomExceptionFilterAttribute : Attribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            var exceptionModel = new CustomErrorViewModel
+            {
+                ActionName = context.ActionDescriptor.DisplayName,
+                Stack = context.Exception.StackTrace,
+                Message = context.Exception.Message
+            };
+            var result = new ViewResult { ViewName = "CustomError" };
+            var modelMetadata = new EmptyModelMetadataProvider();
+            result.ViewData = new ViewDataDictionary<CustomErrorViewModel>(modelMetadata, context.ModelState)
+            {
+                { "CustomException", exceptionModel }
+            };
+
+            context.Result = result;
+            context.ExceptionHandled = true;
+        }
+    }
+}
---
+++ OcsicoTraining.Mikhaltsev/AspApplication/Models/CustomErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OcsicoTraining.Mikhaltsev.Lesson9.AspOrganizations.Models
+{
+    public class CustomErrorViewModel
+    {
+        public string ActionName { get; set; }
+        public string Stack { get; set; }
+        public string Message { get; set; }
+    }
+}
