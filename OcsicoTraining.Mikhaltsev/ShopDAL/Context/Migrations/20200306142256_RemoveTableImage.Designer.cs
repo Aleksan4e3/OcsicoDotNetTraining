b76ce54db6f8c72bafab1a8895b857412183de55
Aleksan4e3 <aleksan4e3@gmail.com>
2020-03-06 17:24:27 +0300

    Remove table Image from DB

--- OcsicoTraining.Mikhaltsev/ShopDAL/Context/Migrations/20200306142256_RemoveTableImage.Designer.cs
+++ OcsicoTraining.Mikhaltsev/ShopDAL/Context/Migrations/20200306142256_RemoveTableImage.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShopDAL.Context;
 
 namespace ShopDAL.Context.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200306142256_RemoveTableImage")]
+    partial class RemoveTableImage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
